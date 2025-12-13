625c852f8709c33c6d28d20c2f3fed2c7d6c3009
krish gusain <krishgusain474@gmail.com>
2025-12-14 00:13:39 +0530

    add python assignment files

---
+++ assignment 1.py
@@ -0,0 +1,12 @@
+#TASK 1
+n=int(input("enter the  first number"))
+x=int(input("enter the  second number"))
+print("the sum is ",n+x)
+print("the difference is ",x-n)
+print("the product is ",n*x)
+print("the division is ",n/x)
+
+#TASK 2
+n=str(input("enter your first name"))
+x=str(input("enter your last name"))
+print("hello",n," ", x,"welcome to the python program")
